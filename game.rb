1198209601546c6187d8f6e1f89c49ab4d2dbe3f
Sanjib Roy <sanjibroy0098@gmail.com>
2020-11-20 05:21:01 +0530

    Make sure player get one more chance if the random value is 6.

--- game.rb
+++ game.rb
@@ -1,5 +1,5 @@
 require_relative "player"
-require 'securerandom'
+require "securerandom"
 
 class Game
   attr_reader :players, :game_id
@@ -11,25 +11,35 @@ class Game
     @game_id = SecureRandom.uuid
     @current_player = 0
     @total_no_of_turns = 0
+    @last_generated_value = 0
 
     player1.game_id = @game_id
     player2.game_id = @game_id
   end
 
+  def status
+    puts "\nGenrated value: #{@last_generated_value}"
+    puts "#{@players[@current_player].name} rolled the dice and his total score is #{@players[@current_player].score}\n\n"
+  end
+
   def handle_turns
-    @current_player = @total_no_of_turns % 2
+    if (@last_generated_value == 6)
+      puts "\n#{@players[@current_player].name} has one more chance!"
+      roll_dice
+    else
+      @total_no_of_turns += 1
+      @current_player = @total_no_of_turns % @players.length
+    end
   end
 
   def roll_dice
-      generated_value = rand(1..6)
-      handle_turns
-      @players[@current_player].score += generated_value
-
-      puts "\nGenrated value: #{generated_value}"
-      puts "#{@players[@current_player].name} rolled the dice and his total score is #{@players[@current_player].score}"
-
-      @total_no_of_turns += 1
+    @last_generated_value = rand(1..6)
+    increment_score
+    status
+    handle_turns
   end
 
-
+  def increment_score
+    @players[@current_player].score += @last_generated_value
+  end
 end
