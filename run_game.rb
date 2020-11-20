df3b28cac3198c7504899279cf92d37a1987f767
Sanjib Roy <sanjibroy0098@gmail.com>
2020-11-20 08:23:36 +0530

    Player will not get any point if he/she scores 3 sixes conscicutively.

--- run_game.rb
+++ run_game.rb
@@ -1,28 +1,35 @@
 require_relative "player"
 require_relative "game"
 
+def separate_by_dash
+    return puts "\n------------------------------------------------------------\n\n"
+end
+
 def get_players
-  print "Enter first player's name:\t"
+  print "\nEnter first player's name:\t"
   name1 = gets.chomp!
 
   print "\nEnter second player's name:\t"
   name2 = gets.chomp!
 
-  if (name1 && name2)
+  if (!name1.empty? && !name2.empty?)
     player1 = Player.new(name1)
     player2 = Player.new(name2)
-    puts "\n------------------------------------------------------------\n\n"
+    
     return [player1, player2]
   else
     puts "\nTwo players are required."
+    separate_by_dash
+    get_players
   end
  
 end
 
 def run_game
   players = get_players
+  separate_by_dash
   puts "#{players[0].name} Vs #{players[1].name}\n\n"
-  puts "\n------------------------------------------------------------\n\n"
+  separate_by_dash
   game = Game.new(true, players[0], players[1])
   is_live = game.will_continue?
   winner = game.get_winner
@@ -33,14 +40,5 @@ def run_game
   end
 end
 
-# puts "\n\nGame_Id: #{game.game_id}\n\n"
-# puts "\n------------------------------------------------------------"
-# game.roll_dice
-# game.roll_dice
-# game.roll_dice
-# game.roll_dice
-# game.roll_dice
-# game.roll_dice
+
 run_game
-# puts "\n------------------------------------------------------------"
-# game.players.each { |player| puts "\nPlayer: #{player.name}\nScore: #{player.score}\n\n" }
