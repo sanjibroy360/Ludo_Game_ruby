28509e6583f1963291aa08071174cd62796f8359
Sanjib Roy <sanjibroy0098@gmail.com>
2020-11-20 07:36:25 +0530

    Game continues until one player wins or quits the game

--- game.rb
+++ game.rb
@@ -3,7 +3,7 @@ require "securerandom"
 
 class Game
   attr_reader :players, :game_id
-  @@total_score = 30
+  @@total_score = 50
 
   def initialize(is_live = true, player1, player2)
     @is_live = is_live
@@ -12,34 +12,73 @@ class Game
     @current_player = 0
     @total_no_of_turns = 0
     @last_generated_value = 0
+    @current_player_name = ""
 
     player1.game_id = @game_id
     player2.game_id = @game_id
   end
 
-  def status
-    puts "\nGenrated value: #{@last_generated_value}"
-    puts "#{@players[@current_player].name} rolled the dice and his total score is #{@players[@current_player].score}\n\n"
+  def current_player_score
+    puts "\nWell done #{@current_player_name}, you have scored #{@last_generated_value}"
+    puts "Now, your total score is #{@players[@current_player].score}\n"
+    puts "\n------------------------------------------------------------\n\n"
+  end
+
+  def instructions
+    @current_player_name = @players[@current_player].name
+    puts "#{players[@current_player].name} it's your turn -" if (@last_generated_value != 6)
+    puts "\nInstructions: "
+    puts "Press enter key to roll the dice."
+    puts "Enter 'exit' to quit the game\n\n"
+  end
+
+  def will_continue?
+    if @is_live
+      instructions
+      player_input = gets
+
+      if (player_input == "\n")
+        return @is_live
+      elsif (player_input.chomp.downcase == "exit")
+        @is_live = false
+      else
+        puts "Please enter a valid input!"
+        will_continue?
+      end
+    end
   end
 
   def handle_turns
     if (@last_generated_value == 6)
-      puts "\n#{@players[@current_player].name} has one more chance!"
+      puts "\n\nHurray...!!!\n#{@current_player_name}, you have got one more chance!"
+      will_continue?
       roll_dice
     else
       @total_no_of_turns += 1
-      @current_player = @total_no_of_turns % @players.length
+      @current_player = (@total_no_of_turns % @players.length)
     end
   end
 
   def roll_dice
     @last_generated_value = rand(1..6)
     increment_score
-    status
+    current_player_score
     handle_turns
   end
 
   def increment_score
-    @players[@current_player].score += @last_generated_value
+    score = @players[@current_player].score + @last_generated_value
+    if (score <= @@total_score)
+      @players[@current_player].score = score
+    end
+  end
+
+  def get_winner
+    winner = @players.select { |player| player.score == @@total_score }[0]
+    if winner
+      puts "#{winner.name} won the game!!!\n\n" 
+      @is_live = false
+    end
+    return winner
   end
 end
--- run_game.rb
+++ run_game.rb
@@ -1,17 +1,46 @@
 require_relative "player"
 require_relative "game"
 
-player1 = Player.new("Sanjib")
-player2 = Player.new("Reettik")
+def get_players
+  print "Enter first player's name:\t"
+  name1 = gets.chomp!
 
-game = Game.new(true, player1, player2)
-puts "\n\nGame_Id: #{game.game_id}\n\n"
-puts "\n------------------------------------------------------------"
-game.roll_dice
-game.roll_dice
-game.roll_dice
-game.roll_dice
-game.roll_dice
-game.roll_dice
-puts "\n------------------------------------------------------------"
-game.players.each {|player| puts "\nPlayer: #{player.name}\nScore: #{player.score}\n\n"}
+  print "\nEnter second player's name:\t"
+  name2 = gets.chomp!
+
+  if (name1 && name2)
+    player1 = Player.new(name1)
+    player2 = Player.new(name2)
+    puts "\n------------------------------------------------------------\n\n"
+    return [player1, player2]
+  else
+    puts "\nTwo players are required."
+  end
+ 
+end
+
+def run_game
+  players = get_players
+  puts "#{players[0].name} Vs #{players[1].name}\n\n"
+  puts "\n------------------------------------------------------------\n\n"
+  game = Game.new(true, players[0], players[1])
+  is_live = game.will_continue?
+  winner = game.get_winner
+  while (winner.nil? && is_live)
+    game.roll_dice
+    winner = game.get_winner
+    is_live = game.will_continue?
+  end
+end
+
+# puts "\n\nGame_Id: #{game.game_id}\n\n"
+# puts "\n------------------------------------------------------------"
+# game.roll_dice
+# game.roll_dice
+# game.roll_dice
+# game.roll_dice
+# game.roll_dice
+# game.roll_dice
+run_game
+# puts "\n------------------------------------------------------------"
+# game.players.each { |player| puts "\nPlayer: #{player.name}\nScore: #{player.score}\n\n" }
